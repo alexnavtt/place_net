397d4c2801e36cb841fad07057b01e95573ffc99
alexnavtt <alexnavtt@utexas.edu>
2024-10-01 16:02:38 -0500

    Refactored into a setuptools package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='base_net',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy<2.0.0',
+        'open3d==0.18.0',
+        'torch',
+        'xacro',
+        'urdf_parser_py'
+    ],
+    entry_points={
+        'console_scripts': [
+            'train_base_net=base_net.train_model:main'
+        ],
+    },
+    package_data={
+        
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: BSD 3-Clause License',
+        'Operating System :: Ubuntu',
+    ],
+    python_requires='>=3.6',
+)
