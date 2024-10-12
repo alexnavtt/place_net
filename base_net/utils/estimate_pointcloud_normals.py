70e6e38af93dad21233feaca5396a3e7d06af765
alexnavtt <alexnavtt@utexas.edu>
2024-10-12 14:31:34 -0500

    Changed behavior when pointcloud already contains normals

--- base_net/utils/estimate_pointcloud_normals.py
+++ base_net/utils/estimate_pointcloud_normals.py
@@ -4,12 +4,9 @@ import argparse
 import numpy as np
 
 def load_arguments():
-    """
-    Load the path to the config file from runtime arguments and load the config as a dictionary
-    """
     parser = argparse.ArgumentParser(
         prog="estimate_pointcloud_normals.py",
-        description="Script to estimate the normals for a generic pointcloud",
+        description="Script to estimate the normals for a generic pointcloud. Can also be used to convert to PCD if you already have normals",
     )
     parser.add_argument('--pointcloud_file', help='Path to pointcloud file to load. Accepts all filetypes compatible with open3d.io.read_point_cloud')
     parser.add_argument('--pitch', default=0.02, type=float, help='voxel size to use for marching cubes')
@@ -24,7 +21,8 @@ def main():
 
     pc = open3d.io.read_point_cloud(args.pointcloud_file)
     if pc.has_normals():
-        print("Pointcloud already has normals, exiting")
+        print("Pointcloud already has normals, writing to output file and exiting")
+        open3d.io.write_point_cloud(args.output_path, pc, write_ascii=True)
         exit(0)
 
     # Convert the pointcloud to a mesh using marching cubes
