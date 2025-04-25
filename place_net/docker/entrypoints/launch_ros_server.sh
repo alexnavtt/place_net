d9a7d76f9e0f69374a44a6b0133b55fccde93723
alexnavtt <alexnavtt@utexas.edu>
2025-04-25 15:39:46 -0600

    Added instructions for adding your own package to the image to reference description files, etc.

--- place_net/docker/entrypoints/launch_ros_server.sh
+++ place_net/docker/entrypoints/launch_ros_server.sh
@@ -3,4 +3,8 @@
 source /opt/ros/humble/setup.bash
 source /colcon_ws/install/setup.bash
 
-ros2 run place_net_ros place_net_server --ros-args --params-file /place_net_ros_params.yaml  -p checkpoint_path:=/place_net_model.pt
+if [ -f "/guest_ws/install/setup.bash" ]; then
+    source /guest_ws/install/setup.bash
+fi
+
+ros2 run place_net_ros place_net_server --ros-args --params-file /place_net_ros_params.yaml -p checkpoint_path:=/place_net_model.pt
