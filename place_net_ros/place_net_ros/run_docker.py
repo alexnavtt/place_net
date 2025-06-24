47b61dfad51d10bac59ea04dfcbaeacf89d4af09
alexnavtt <alexnavtt@utexas.edu>
2025-06-24 10:33:16 -0600

    Fixed issue with dangling docker image

--- place_net_ros/place_net_ros/run_docker.py
+++ place_net_ros/place_net_ros/run_docker.py
@@ -1,4 +1,5 @@
 import os
+import atexit
 import subprocess
 from ament_index_python import get_package_share_directory
 
@@ -6,7 +7,12 @@ def main():
     this_pkg_share = get_package_share_directory('place_net_ros')
     with open(os.path.join(this_pkg_share, 'source_directory.txt')) as f:
         docker_path = f.read()
-    subprocess.run(['docker', 'compose', 'run', '--rm', 'ros_server'], cwd=docker_path)
+    
+    def shutdown_callback():
+        subprocess.run(['docker', 'container', 'stop', 'place_net_server_process'])
+    atexit.register(shutdown_callback)
+    
+    subprocess.run(['docker', 'compose', 'run', '--name', 'place_net_server_process', '--rm', 'ros_server'], cwd=docker_path)
 
 if __name__ == '__main__':
     main()
